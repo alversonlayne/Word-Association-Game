71dfebfd986e5680b3d739673c32b7beead2171a
Alverson Layne <alverson.layne2@gmail.com>
2022-07-06 17:31:18 -0400

    questions constant hardcoded

---
+++ app.js
@@ -0,0 +1,20 @@
+const scoreDisplay = document.getElementById('score-display')
+const questionDisplay = document.getElementById('question-display')
+
+const questions = [
+    {
+        quiz: ['value', 'estimate', 'evaluate'],
+        options: ['jury', 'assess'],
+        correct: 2
+    },
+    {
+        quiz: ['close', 'near', 'next'],
+        options: ['forecast', 'sustainable'],
+        correct: 1
+    },
+    {
+        quiz: ['fast', 'quick', 'prompt'],
+        options: ['charity', 'rapid'],
+        correct: 2
+    },
+]
